e19cdb803845c85f77e22cbb90b80b73e8a34cb7
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-05-27 01:05:58 +0700

    feat: Render note

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useDrop } from 'react-dnd'
 
 import Types from 'Constants/ReactDndTypes'
+import Note from 'Components/Note/Note'
 
 const Container = () => {
+	const [Notes] = useState([
+		{
+			id: '6668fd28-9d52-4dae-8572-4ce2c1135a92',
+			left: 0,
+			top: 0,
+			notes: ['Note 1', 'Note 2'],
+		},
+	])
+
 	const [, drop] = useDrop(
 		() => ({
 			accept: Types.NOTE,
@@ -19,7 +29,15 @@ const Container = () => {
 				width: '100vw',
 				height: '100vh',
 			}}
-		></div>
+		>
+			{Notes.map(({ id, left, top, notes }) => {
+				return (
+					<Note key={id} left={left} top={top}>
+						{notes.toString()}
+					</Note>
+				)
+			})}
+		</div>
 	)
 }
 
