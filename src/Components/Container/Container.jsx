6e013303e1206a8dd06351cbd80ed83d9fcfd41f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-05-27 01:21:09 +0700

    feat: Move note

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -6,20 +6,35 @@ import Types from 'Constants/ReactDndTypes'
 import Note from 'Components/Note/Note'
 
 const Container = () => {
-	const [Notes] = useState([
-		{
-			id: '6668fd28-9d52-4dae-8572-4ce2c1135a92',
+	const [Notes, SetNotes] = useState({
+		'6668fd28-9d52-4dae-8572-4ce2c1135a92': {
 			left: 0,
 			top: 0,
 			notes: ['Note 1', 'Note 2'],
 		},
-	])
+	})
+
+	const MoveNote = (id, left, top) => {
+		SetNotes(notes => {
+			return { ...notes, [id]: { ...notes[id], left, top } }
+		})
+	}
 
 	const [, drop] = useDrop(
 		() => ({
 			accept: Types.NOTE,
+			drop: (item, monitor) => {
+				const delta = monitor.getDifferenceFromInitialOffset()
+
+				const left = item.left + delta.x
+				const top = item.top + delta.y
+
+				MoveNote(item.id, left, top)
+
+				return
+			},
 		}),
-		[]
+		[MoveNote]
 	)
 
 	return (
@@ -30,9 +45,11 @@ const Container = () => {
 				height: '100vh',
 			}}
 		>
-			{Notes.map(({ id, left, top, notes }) => {
+			{Object.keys(Notes).map(id => {
+				const { left, top, notes } = Notes[id]
+
 				return (
-					<Note key={id} left={left} top={top}>
+					<Note key={id} id={id} left={left} top={top}>
 						{notes.toString()}
 					</Note>
 				)
