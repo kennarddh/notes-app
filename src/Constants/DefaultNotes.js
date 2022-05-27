91933e7c80f6635dc94a942fd3a831b47231233b
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-05-27 15:53:39 +0700

    feat: Default notes

---
+++ src/Constants/DefaultNotes.js
@@ -0,0 +1,12 @@
+const DefaultNotes = {
+	'6668fd28-9d52-4dae-8572-4ce2c1135a92': {
+		left: 0,
+		top: 0,
+		notes: [
+			{ id: 'c4a8dc18-08b2-4ffc-8a4e-4e7d838f2c94', note: 'Note 1' },
+			{ id: '4ad0f9dd-a3c3-4cc8-8ab4-6853b354a05c', note: 'Note 2' },
+		],
+	},
+}
+
+export default DefaultNotes
