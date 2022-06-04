65e01088f4ee79702d964d6ef7e90bc8eb1cfc78
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-04 10:17:08 +0700

    refactor: Is touch device

--- src/Utils/IsTouchDevice.js
+++ src/Utils/IsTouchDevice.js
@@ -1,10 +1,4 @@
 // simple way to check whether the device support touch (it doesn't check all fallback, it supports only modern browsers)
-const IsTouchDevice = () => {
-	if ('ontouchstart' in window) {
-		return true
-	}
-
-	return false
-}
+const IsTouchDevice = () => 'ontouchstart' in window
 
 export default IsTouchDevice
