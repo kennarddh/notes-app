741835996269d0d72543663c445b09968445ed88
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-04 09:25:49 +0700

    feat: Multiple react dnd backend

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react'
 
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { TouchBackend } from 'react-dnd-touch-backend'
 
 import Container from 'Components/Container/Container'
 import NotesProvider from 'Contexts/Notes'
 import ThemeProvider from 'Contexts/Theme'
 
+import IsTouchDevice from 'Utils/IsTouchDevice'
+
 const App = () => {
 	return (
-		<DndProvider backend={HTML5Backend}>
+		<DndProvider backend={IsTouchDevice() ? TouchBackend : HTML5Backend}>
 			<ThemeProvider>
 				<NotesProvider>
 					<Container />
